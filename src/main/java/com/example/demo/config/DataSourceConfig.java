5a8ebf4dbcd58be4d09adae4901300cd7379cf6c
Your Name <sabyasachibehura3@gmail.com>
2026-02-02 21:45:53 +0530

    modified datasource

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -9,12 +9,17 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+
 import com.zaxxer.hikari.HikariDataSource;
+
+import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
@@ -64,6 +69,12 @@ public class DataSourceConfig {
             return emf;
     }
     
+    @Bean
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
     
 }
 
